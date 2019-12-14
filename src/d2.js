fe898c8904a123a3f7048e2c68dd815282a8ad86
Jim Newbery <james@tinnedfruit.com>
2019-12-14 10:36:26 +0000

    Day 2 part 1 tests

--- src/d2.js
+++ src/d2.js
@@ -2,7 +2,7 @@ const Intcode = require('./intcode.js');
 
 function run(data) {
   let program = parse(data);
-  console.log('Day 2:1: ', part1(program));
+  console.log('Day 2:1: ', part1(program, 12, 2));
 }
 
 function parse(data) {
@@ -12,12 +12,17 @@ function parse(data) {
     .map(Number);
 }
 
-function part1(program) {
+function part1(program, p1, p2) {
   const computer = new Intcode();
   computer.load(program);
-  computer.setMem(1, 12);
-  computer.setMem(2, 2);
-  return computer.run()[0];
+  if (p1) {
+    computer.setMem(1, p1);
+  }
+  if (p2) {
+    computer.setMem(2, p2);
+  }
+  computer.run();
+  return computer.memory[0];
 }
 
 module.exports = { run, parse, part1 };
--- tests/d2.js
+++ tests/d2.js
@@ -1,1 +1,9 @@
 const test = require('tape');
+const d2 = require('../src/d2.js');
+
+test('Day 2 part 1', t => {
+  t.plan(3);
+  t.equal(d2.part1([1, 0, 0, 0, 99]), 2);
+  t.equal(d2.part1([1, 1, 1, 4, 99, 5, 6, 0, 99]), 30);
+  t.equal(d2.part1([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]), 3500);
+});
