30c7b231eb1e4e0124cad361074299b8b64be259
Jim Newbery <james@tinnedfruit.com>
2019-12-19 19:58:29 +0000

    Day 5 part 2

--- src/d5.js
+++ src/d5.js
@@ -3,6 +3,7 @@ const Intcode = require('./intcode.js');
 function run(data) {
   let program = parse(data);
   console.log('Day 5:1: ', part1(program, 1).reverse()[0]);
+  console.log('Day 5:2: ', part2(program, 5).reverse()[0]);
 }
 
 function parse(data) {
@@ -20,4 +21,12 @@ function part1(program, input) {
   return computer.output;
 }
 
-module.exports = { run, part1 };
+function part2(program, input) {
+  const computer = new Intcode();
+  computer.load(program);
+  computer.input = input;
+  computer.run();
+  return computer.output;
+}
+
+module.exports = { run, part1, part2 };
